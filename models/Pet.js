19a2e6cd20e83cdb787062b9ba626d1c62ce4561
Olivia McKee <olivia.mckee97@gmail.com>
2022-08-01 18:25:29 -0500

    server connection and started pet model

---
+++ models/Pet.js
@@ -0,0 +1,26 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Pet extends Model {}
+
+Pet.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    owner_id: {
+      type: DataTypes.INTEGER,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "post",
+  }
+);
+
+module.exports = Pet;
