4ed913f7acca98f42379f68d1183e240ce93df93
NickSchenck <yoyo4games@yahoo.com>
2022-08-04 16:12:17 -0500

    added more front end and route funtionality

--- public/javascript/sign-up.js
+++ public/javascript/sign-up.js
@@ -1,25 +1,31 @@
 async function signupFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const username = document.querySelector("").value.trim();
-    const email = document.querySelector("").value.trim();
-    const password = document.querySelector("").value.trim();
+  const name = document.querySelector('#full-name').value.trim();
+  const username = document.querySelector("#username").value.trim();
+  const email = document.querySelector("#email").value.trim();
+  const password = document.querySelector("#password").value.trim();
+  const phone = document.querySelector("#phone").value.trim();
 
-    if (username && email && password) {
-        const response = await fetch('/api/users', {
-          method: 'post',
-          body: JSON.stringify({
-            username,
-            email,
-            password
-          }),
-          headers: { 'Content-Type': 'application/json' }
-        });
-        //check the response status
-        if (response.ok) {
-          console.log("success");
-        }else{
-          alert(response.statusText);
-        }
-      }
-    };
+  if (name && username && email && password && phone) {
+    const response = await fetch('/api/users', {
+      method: 'post',
+      body: JSON.stringify({
+        name,
+        username,
+        email,
+        password,
+        phone
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+    //check the response status
+    if (response.ok) {
+      console.log("success");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document.querySelector(".sign-up-form").addEventListener("submit", signupFormHandler);
